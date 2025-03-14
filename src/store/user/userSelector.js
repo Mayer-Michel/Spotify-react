6f05f63adf274f61f5732fdd19d0a38c6bfeeca6
Mayer MICHEL <mayer.mohib@gmail.com>
2025-03-14 16:42:05 +0100

    preparation pour userDetail

--- src/store/user/userSelector.js
+++ src/store/user/userSelector.js
@@ -2,10 +2,12 @@ import { createSelector } from "@reduxjs/toolkit";
 
 const selectLoading = (state) => state.users.loading;
 const selectUserFavorites = (state) => state.users.userFavorites;
+const selectUserDetail = (state) => state.users.userDetail;
+const selectAvatars = (state) => state.users.avatars;
 
 const selectUserData = createSelector(
-    [selectLoading, selectUserFavorites],
-    (loading, userFavorites) => ({loading, userFavorites})
+    [selectLoading, selectUserFavorites, selectUserDetail, selectAvatars],
+    (loading, userFavorites, userDetail, avatars) => ({loading, userFavorites, userDetail, avatars})
 );
 
 export default selectUserData;
--- src/store/user/userSlice.js
+++ src/store/user/userSlice.js
@@ -1,37 +1,70 @@
 import { createSlice } from "@reduxjs/toolkit";
-import { API_URL } from "../../constants/apiConstant";
 import axios from "axios";
-
+import { API_URL } from "../../constants/apiConstant";
 
 const userSlice = createSlice({
-    name: "users",
-    initialState:{
-        loading:false,
-        userFavorites: []
+  name: "users",
+  initialState:{
+    loading:false,
+    userDetail: {},
+    userFavorites: [],
+    avatars: [],
+  },
+  reducers: {
+    setLoading: (state, action) => {
+      state.loading = action.payload;
     },
-    reducers: {
-        setLoading: (state, action) => {
-            state.loading = action.payload;
-        },
-        setUserFavorites: (state, action) => {
-            state.userFavorites = action.payload;
-        }
+    setUserDetail: (state, action) => {
+      state.userDetail = action.payload;
+    },
+    setUserFavorites: (state, action) => {
+      state.userFavorites = action.payload;
+    },
+    setAvatars: (state, action) => {
+      state.avatars = action.payload['hydra:member'];
     }
+  }
 })
 
-export const { setLoading, setUserFavorites } = userSlice.actions;
+export const { setLoading,setUserDetail, setUserFavorites, setAvatars } = userSlice.actions;
 
-// Méthode qui récupère les favoris d'un utilisateur
-export const fetchUserFavorites = (userId) => async (dispatch) => {
-    try {
-        dispatch(setLoading(true));
-        const response = await axios.get(`${API_URL}/users/${userId}`)
-        dispatch(setUserFavorites(response.data.albums));
-    } catch (error) {
-        console.log(`erreur lors du fetchUserFavorites : ${error}`);
-    }finally{
-        dispatch(setLoading(false));
-    }
-} 
+//méthode qui récupère les info d'un user
+export const fetchUserDetail = (id) => async (dispatch) => {
+  try {
+    dispatch(setLoading(true));
+    const response = await axios.get(`${API_URL}/users/${id}`);
+    dispatch(setUserDetail(response.data));
+  } catch (error) {
+    console.log(`erreur lors du fetchUserDetail : ${error}`);
+  }finally{
+    dispatch(setLoading(false));
+  }
+}
+
+//méthode qui recupère les favories d'un utilisateur
+export const fetchUserFavorites = (userId) => async (dispatch) =>{
+  try {
+    dispatch(setLoading(true));
+    const response = await axios.get(`${API_URL}/users/${userId}`)
+    dispatch(setUserFavorites(response.data.albums));
+  } catch (error) {
+    console.log(`erreur lors du fetchUserFavorites : ${error}`);
+  }finally{
+    dispatch(setLoading(false));
+  }
+}
+
+//méthode qui récupère tous les avatars
+export const fetchAvatars = () => async (dispatch) => {
+  try {
+    dispatch(setLoading(true));
+    const response = await axios.get(`${API_URL}/avatars?page=1&isActive=true`);
+    dispatch(setAvatars(response.data));
+  } catch (error) {
+    console.log(`erreur lors du fetchAvatars : ${error}`);
+  }finally{
+    dispatch(setLoading(false));
+  }
+}
 
 export default userSlice.reducer;
